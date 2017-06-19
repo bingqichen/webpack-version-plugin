baada1204ae4c303b7d91ce6f802375b792d34d9
bingqichen <bingqichen@live.cn>
2017-06-19 18:35:51 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 function WebpackVersionPlugin(opts) {
-
+  if (typeof opts.cb !== 'function') {
+    throw new Error('You must set the cb option');
+  }
+  this.opts = opts;
 }
 
 WebpackVersionPlugin.prototype.apply = function(compiler, callback) {
+  const self = this;
   compiler.plugin('emit', function(compilation, callback) {
-    console.log(compilation.getStats());
+    const hashMap = {
+      hash: compilation.fullHash,
+      chunkHash: {}
+    };
+
+    compilation.chunks.forEach(function (item) {
+      hashMap.chunkHash[item.name] = item.hash;
+    });
+
+    self.opts.cb(hashMap);
+
     callback();
   });
-}
+};
 
-module.exports = WebpackVersionPlugin
+module.exports = WebpackVersionPlugin;
